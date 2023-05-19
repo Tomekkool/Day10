cc792412746477077e4066117c2b687bc9b99d5c
Tomek <Tomek@KOMPUTER>
2023-05-19 11:33:22 +0200

    Add project files.

---
+++ P01PlayersMVCWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P01PlayersMVCWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(IFormCollection form)
+        {
+            int number1 = int.Parse(form["number1"]);
+            int number2 = int.Parse(form["number2"]);
+
+            int result= number1 + number2;
+
+            ViewBag.Result = result;
+            return View("Index");
+        }
+
+    }
+}
---
+++ P01PlayersMVCWebApp/Controllers/TestingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using P01PlayersMVCWebApp.Models;
+
+namespace P01PlayersMVCWebApp.Controllers
+{
+    public class TestingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SampleText()
+        {
+
+            ViewBag.MyOwnMessage = "hello from controller";
+            return View();
+        }
+
+        public IActionResult PersonsList()
+        {
+            var persons = new List<Person>()
+            {
+            new Person() {Id=1, Name="a",Age=1},
+            new Person() {Id=2, Name="b",Age=1},
+            new Person() {Id=3, Name="c",Age=1},
+
+            };
+            
+            return View(persons);
+        }
+        public IActionResult NewPersonsList()
+        {
+            var persons = new List<Person>()
+            {
+            new Person() {Id=1, Name="a",Age=1},
+            new Person() {Id=2, Name="b",Age=1},
+            new Person() {Id=3, Name="c",Age=1},
+
+            };
+
+            return View(persons);
+        }
+
+    }
+}
